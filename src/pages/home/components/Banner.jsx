a34ad0923943459bb1b762c51868e0616e06eaef
ade-ryzki <ade.ryzki@gmail.com>
2022-11-23 14:47:31 +0700

    caroseil

--- src/pages/home/components/Banner.jsx
+++ src/pages/home/components/Banner.jsx
@@ -7,7 +7,7 @@ const partOne = () => {
 	return (
         
     <>
-    <div>
+    {/* <div>
         <div className="w-full carousel rounded-sm ">
             <div className="carousel-item w-full h-96">
                 <img src={banner} className="w-full" alt="Tailwind CSS Carousel component" />
@@ -18,6 +18,29 @@ const partOne = () => {
             <div className="carousel-item w-full h-96">
             <img src={banner2} className="w-full" alt="Tailwind CSS Carousel component" />
             </div> 
+        </div>
+    </div> */}
+   <div className="carousel w-full h-1/2">
+        <div id="slide1" className="carousel-item relative w-full">
+            <img src={banner} className="w-full" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle bg-slate-600 ">❮</a> 
+            <a href="#slide2" className="btn btn-circle bg-slate-600">❯</a>
+            </div>
+        </div> 
+        <div id="slide2" className="carousel-item relative w-full">
+            <img src={banner3} className="w-full" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle bg-slate-600">❮</a> 
+            <a href="#slide3" className="btn btn-circle bg-slate-600">❯</a>
+            </div>
+        </div> 
+        <div id="slide3" className="carousel-item relative w-full">
+            <img src={banner2} className="w-full" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle bg-slate-600">❮</a> 
+            <a href="#slide1" className="btn btn-circle bg-slate-600">❯</a>
+            </div>
         </div>
     </div>
     </>
