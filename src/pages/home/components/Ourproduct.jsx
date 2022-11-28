db612424ea2b50c472945e8f22506faaf3c09425
ade-ryzki <ade.ryzki@gmail.com>
2022-11-28 16:33:29 +0700

    our product

--- src/pages/home/components/Ourproduct.jsx
+++ src/pages/home/components/Ourproduct.jsx
@@ -11,7 +11,7 @@ const parttwo = () => {
             <img src={Wifi} className="max-w-sm rounded-lg shadow-2xl" alt=''/>
             <div>
             <h1 className="text-5xl font-bold">Menghubungkan Jaringan dan Andal!</h1>
-            <p className="py-6">CIC merupakan perusahaan <a className="font-semibold">Internet Service Provider</a> yang menyediakan berbagai solusi IT lengkap seperti internet, IPTV, hosting, dan beberapa produk lainnya. Untuk mengetahui produk apa saja yang ada di CIC, silahkan klik tombol "Product"</p>
+            <p className="py-6 text-lg text-justify">CIC merupakan perusahaan <a className="font-semibold">Internet Service Provider</a> yang menyediakan berbagai solusi IT lengkap seperti internet, IPTV, hosting, dan beberapa produk lainnya. Untuk mengetahui produk apa saja yang ada di CIC, silahkan klik tombol "Product"</p>
             <Link to='/product'>
             <button className="btn transition ease-in-out delay-150 bg-[#9ACD32] bg-opacity-70 hover:-translate-y-1  hover:bg-[#FFBA33] hover:bg-opacity-70 duration-300 rounded-2xl">Our Product</button>
             </Link>
